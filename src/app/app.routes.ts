10b380c417866f04c6317d6828bc4d1fc79afb01
Nikita Gawde <gawdenikita10@gmail.com>
2025-05-20 23:25:38 +0530

    fixing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,35 @@ import { UsersComponent } from './users/users.component';
 import { UserRolesComponent } from './users/user-roles/user-roles.component';
 import { UserPermissionsComponent } from './users/user-permissions/user-permissions.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './guards/auth.guard';
+import { CustomersComponent } from './loan-assessment/customers/customers.component';
+import { UploadDocumentsComponent } from './loan-assessment/upload-documents/upload-documents.component';
+import { WorkOrdersComponent } from './loan-assessment/work-orders/work-orders.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeNavComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'roles', component: UserRolesComponent },
-  { path: 'permissions', component: UserPermissionsComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuard] },
+  { path: 'roles', component: UserRolesComponent, canActivate: [AuthGuard] },
   { path: '**', component: PageNotFoundComponent },
+  {
+    path: 'permissions',
+    component: UserPermissionsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'upload-documents',
+    component: UploadDocumentsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'customers',
+    component: CustomersComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'work-orders',
+    component: WorkOrdersComponent,
+    canActivate: [AuthGuard],
+  },
 ];
