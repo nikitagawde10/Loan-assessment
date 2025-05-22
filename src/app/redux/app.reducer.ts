6ccd8c97564471f564fa9651d52272108074217a
Nikita Gawde <gawdenikita10@gmail.com>
2025-05-22 14:55:28 +0530

    removing unused files

--- src/app/redux/app.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { ActionReducerMap } from '@ngrx/store';
-import { AppState } from './app.state';
-import { customersReducer } from './customers/customers.reducer';
-import { userRolesReducer } from './roles/roles.reducer';
-
-export const reducers: ActionReducerMap<AppState> = {
-  customers: customersReducer,
-  userRoles: userRolesReducer,
-};
--- src/app/redux/app.state.ts
+++
@@ -1,7 +0,0 @@
-import { CustomersState } from './customers/customers.state';
-import { UserRolesState } from './roles/roles.state';
-
-export interface AppState {
-  customers: CustomersState;
-  userRoles: UserRolesState;
-}
