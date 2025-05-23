423d8ce03f46e9e85246c29784d1da5ded2dd6c0
Nikita Gawde <gawdenikita10@gmail.com>
2025-05-23 13:17:36 +0530

    minor bug fix

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,13 +5,14 @@ import { loginSuccess, logout } from './redux/login/login.action';
 import { selectUser } from './redux/user/user.selectors';
 import { selectLoggedInUser } from './redux/login/login.selectors';
 import { take, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private store = inject(Store);
-
+  private router = inject(Router);
   constructor() {}
 
   login(email: string, password: string): Observable<boolean> {
@@ -31,11 +32,10 @@ export class AuthService {
   }
 
   logout(): void {
-    // Dispatch logout action
     this.store.dispatch(logout());
+    this.router.navigate(['/login'], { replaceUrl: true });
   }
 
-  // Method to get the logged-in user's email from the store
   getUserEmail(): Observable<string | null> {
     return this.store.select(selectLoggedInUser).pipe(
       map((user) => (user ? user.email : null)) // Map the user object to the email or null
