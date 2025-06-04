3ae56e16b40ea43038efa1168e0f350dd9074666
Nikita Gawde <gawdenikita10@gmail.com>
2025-06-04 12:15:11 +0530

    Removing view child

--- src/app/users/user-roles/user-roles.component.ts
+++ src/app/users/user-roles/user-roles.component.ts
@@ -1,12 +1,7 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatTable,
-  MatTableDataSource,
-  MatTableModule,
-} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-import { Store } from '@ngrx/store';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateRoleComponent } from './create-role/create-role.component';
 import { MatIcon } from '@angular/material/icon';
@@ -22,9 +17,8 @@ import { UserRoleService } from './user-role.service';
   styleUrl: './user-roles.component.css',
 })
 export class UserRolesComponent implements OnInit {
-  @ViewChild(MatTable) table!: MatTable<UserRole>;
   roleService = inject(UserRoleService);
-  constructor(private store: Store, private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog) {}
 
   displayedColumns: string[] = ['roleName', 'protected', 'actions'];
 
@@ -39,9 +33,9 @@ export class UserRolesComponent implements OnInit {
       width: '600px',
       disableClose: true,
     });
-
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
+        console.log(result);
         this.roleService.addUserRole(result);
       }
     });
